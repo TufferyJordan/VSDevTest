1f461a242049433c6b46ce195c05977f25d886d2
Jordan Tuffery <tufferyjordan@gmail.com>
2020-05-27 17:23:52 +0200

    finished test

--- Assets/Scripts/Test/Tester.cs
+++ Assets/Scripts/Test/Tester.cs
@@ -11,20 +11,27 @@ namespace Test
 
         public Button startGameButton;
 
-        public Button endGameButton; 
+        public Button endGameButton;
+
+        public Button GDPRConsentConfirmButton;
+
+        public Toggle GDPRConsentToggle;
+
+        public GameObject GDPRPanel;
 
         private void Awake()
         {
             showAdButton.onClick.AddListener(ShowAdClick);
             startGameButton.onClick.AddListener(StartGameClick);
             endGameButton.onClick.AddListener(EndGameClick);
-            
+            GDPRConsentConfirmButton.onClick.AddListener(GDPRConsentConfirm);
+
             VoodooSauce.SetAdDisplayConditions(60, 3); 
         }
 
         private void ShowAdClick()
         {
-            VoodooSauce.ShowAd(); 
+            VoodooSauce.ShowAd("f4280fh0318rf0h2"); 
         }
 
         private void StartGameClick()
@@ -35,6 +42,12 @@ namespace Test
         private void EndGameClick()
         {
             VoodooSauce.EndGame(); 
+        }
+
+        private void GDPRConsentConfirm()
+        {
+            GDPRPanel.SetActive(false);
+            VoodooSauce.SetGDPRConsent(GDPRConsentToggle.isOn);
         }
     }
 }
--- Assets/Scripts/VoodooSauce.cs
+++ Assets/Scripts/VoodooSauce.cs
@@ -22,36 +22,74 @@
  * 
  */
 
- // Bonus Question : Show an android Toast when you launch the app.
+// Bonus Question : Show an android Toast when you launch the app.
+
+using _3rdParty;
+using System;
 
 
 public static class VoodooSauce{
-	
+
 	// Before calling methods in TopAds and TopAnalytics you must call their init methods 
 	// TopAds requires the TopAds prefab to be created in the scene
 	// You also need to collect user GDPR consent and pass that boolean value to TopAds and TopAnalytics 
-	// You can collect this consent by displaying a popup to the user at the start of the game and then storing that value for future use 
-	
-	public static void StartGame()
+	// You can collect this consent by displaying a popup to the user at the start of the game and then storing that value for future use
+
+	private static int secondsBetweenAds = 0;
+	private static int gamesBetweenAds = 0;
+
+	private static DateTime lastTimeAdShown = DateTime.MinValue;
+	private static int gamesShown = 0;
+
+    public static void StartGame()
 	{
+		gamesShown++;
+		TopAnalytics.TrackEvent("gameStarted");
 		// Track in TopAnalytics that a game has started 
 	}
 
 	public static void EndGame()
 	{
+		TopAnalytics.TrackEvent("gameEnded");
 		// Track in TopAnalytics that a game has ended 
 	}
 	
-	public static void ShowAd()
+	public static void ShowAd(string adId)
 	{
 		// TopAds methods must be called with a unique "string" ad unit id 
 		// For your test app that id is "f4280fh0318rf0h2" 
 		// However, when releasing the SDK to other studios, their ad unit id will be different 
-		// Please find a flexible way to allow studios to provide their ad unit id to your VoodooSauce SDK 
-		
-		
+		// Please find a flexible way to allow studios to provide their ad unit id to your VoodooSauce SDK
+
 		// Before an ad is available to display, you must call TopAds.RequestAd 
-		// You must call RequestAd each time before an ad is ready to display 
+		// You must call RequestAd each time before an ad is ready to display
+		TopAds.OnAdLoadedEvent += delegate ()
+		{
+			System.Diagnostics.Debug.WriteLine("The ad is loaded");
+
+			TopAds.ShowAd(adId);
+		};
+
+		TopAds.OnAdShownEvent += delegate ()
+		{
+			System.Diagnostics.Debug.WriteLine("The ad has been shown");
+
+			lastTimeAdShown = DateTime.Now;
+			gamesShown = 0;
+		};
+
+		TopAds.OnAdFailedEvent += delegate ()
+		{
+			System.Diagnostics.Debug.WriteLine("The ad failed to load or show, please refer to the SDK documentation for further details");
+		};
+
+		DateTime lastTime = lastTimeAdShown.AddSeconds(Convert.ToDouble(secondsBetweenAds));
+		if (DateTime.Now.CompareTo(lastTime) >= 0
+            && gamesShown >= gamesBetweenAds)
+        {
+			TopAds.RequestAd(adId);
+
+		}
 
 		// RequestAd will make a "fake" request for an ad that will take 0 to 10 seconds to complete
 		// Afterwards, either the OnAdLoadedEvent or OnAdFailedEvent will be invoked 
@@ -60,37 +98,80 @@ public static class VoodooSauce{
 
 		// If an ad is loaded correctly, clicking on the "Show Ad" button within Unity-VoodooSauceTestApp 
 		// should display a fake ad popup that you can close. 
-		
+
 
 		// Track in TopAnalytics when an ad is displayed.  Hint: TopAds.OnAdShownEvent 
 	}
 
 	public static void SetAdDisplayConditions(int secondsBetweenAds, int gamesBetweenAds)
 	{
+		VoodooSauce.secondsBetweenAds = secondsBetweenAds;
+		VoodooSauce.gamesBetweenAds = gamesBetweenAds;
+		VoodooSauce.gamesShown = gamesBetweenAds;
 		// Sometimes studios call "ShowAd" too often and bombard players with ads 
 		// Add a system that prevents the "ShowAd" method from showing an available ad 
 		// Unless EITHER condition provided is true: 
 		// 1) secondsBetweenAds: only show an ad if the previous ad was shown more than "secondBetweenAds" ago 
 		// 2) gamesBetweenAds: only show an ad if "gamesBetweenAds" amount of games was played since the previous ad 
 	}
-	
-	
-	// === Please answer these quick questions within the code file ===  
-	
+
+    public static void SetGDPRConsent(bool gdprConsent)
+    {
+		TopAnalytics.InitWithConsent(gdprConsent);
+        
+		TopAds.InitializeSDK();
+        if(gdprConsent)
+        {
+			TopAds.GrantConsent();
+		} else
+        {
+			TopAds.RevokeConsent();
+        }
+	}
+
+
+	// === Please answer these quick questions within the code file ===
+
 	// In the VoodooSauce we integrate many 3rd party SDKs of varying reliability that display Ads, Analytics, etc.
 	// What processes would you suggest to ensure that the VoodooSauce SDK is minimally affected by crashes 
 	// in another SDK?
-	
+
+	// I would suggest doing a huge number of unit tests, use an agile methodology to shorten the releases, and fix bug faster.
+	// I would also suggest isolating every SDK added using interfaces to decouple the code architecture to be able to tests the code easier and faster.
+	// Then I would suggest that the sdk catch every crashes that might occur during these 3rd party sdk usage.
+
+
+
 	// What are some pitfalls/shortcomings in your above implementation?
-	
-	// How would you improve your implementation if you had more than 2 hours? 
-	
-	// What do you enjoy the most about being a developer? 
-	
-	// What do you enjoy the least about being a developer? 
-	
+
+	// I hadn't the time to unit test my code so I'm afraid it would not be robust. I also didn't take the time to make the SDK clearer
+	// and it could make the users use bad implementations.
+
+
+
+	// How would you improve your implementation if you had more than 2 hours?
+
+	// I would have added unit tests, I would have also caught each error that might occur in the third party sdk
+	// to process them and deliver better errors understanding to the user.
+
+
+
+	// What do you enjoy the most about being a developer?
+
+	// I think architecture is the most entertaining task in my job. Ease the life
+	// of the other developers in my team is the best reward I can get.
+
+
+
+	// What do you enjoy the least about being a developer?
+
+	// The worst thing that my job requires me to do is to sometimes work with pressure to fulfill an agenda. The problem is that
+	// when you shorten the time, you also lessen quality. And this could lead to bad implementations, bugs, and bad mood in the team.
+
+
+
 	// Why do you want to work on the VoodooSauce SDK vs. creating games in Unity?
-	
-	
-	
+
+	// I've already created some games in Unity, and that was fun. But I've never tried writing something that other developers could use.
+	// I think this kind of experience could bring me a deeper knowledge in my job and I would love that.
 }
